9195ca0319109d2513c652c8a0c12e9be374e9a6
Jason <jason@goecke.net>
2010-10-01 08:29:04 -0700

    Minor changes with the rename to get unit tests to pass.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,7 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'agi_config'))
 
 @tropo_testing = true
-%w(rubygems fakeweb eventmachine tropo-agi lib/tropo em-spec/rspec yaml).each { |lib| require lib }
+%w(rubygems fakeweb eventmachine tropo-agitate lib/tropo em-spec/rspec yaml).each { |lib| require lib }
 
 Spec::Runner.configure do |config|
 
